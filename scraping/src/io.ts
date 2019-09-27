48d8c6607bd9f954db33b568ea88e9e3d94da2ed
watanabeJunna <junna7897@icloud.com>
2019-09-27 17:09:08 +0900

    add io.ts

---
+++ scraping/src/io.ts
@@ -0,0 +1,22 @@
+import { writeFileSync, appendFile } from 'fs';
+
+export namespace File {
+    // default path
+    export const defpath = process.env.DIST_FILEPATH;
+
+    export const initializeFile = (path: string = defpath): void => {
+        writeFileSync(path, '', 'utf8');
+    }
+
+    export const writeToCsv = (collection: string[][], path: string = defpath): void => {
+        const contents: string = collection.map<string>(c => {
+            return Object.values(c).join(',') + '\n';
+        }).join('');
+
+        appendFile(path, contents, 'utf8', (e: Error) => {
+            if (e) {
+                console.log(e);
+            }
+        });
+    }
+}
